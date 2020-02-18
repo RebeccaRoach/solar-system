23c8c3b6c25ec91545d045efbbd8a7532afdeb13
RebeccaRoach <rebeccaroach12@gmail.com>
2020-02-18 13:37:02 -0800

    Wave 1 for Planet Constructor

---
+++ planet.rb
@@ -0,0 +1,15 @@
+class Planet
+
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
+  end
+
+  # def summary
+
+end
