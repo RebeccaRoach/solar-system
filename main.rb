88fbccced9e8f662a68683f9e866b59edad4ce66
RebeccaRoach <rebeccaroach12@gmail.com>
2020-02-18 15:38:46 -0800

    Fixed Wave 1 so summary method is in Planet class.

--- main.rb
+++ main.rb
@@ -1,15 +1,37 @@
 require_relative 'planet'
+require_relative 'solar-system'
 
+# Wave 1
 def main
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
   mercury = Planet.new('Mercury', 'red', 5.972e24, 1.496e8, 'It is the smallest planet?')
-
-  def summary
-    return "For the #{@color} planet #{@name}, it has a mass of #{@mass_kg} kgs."
-  end
 
   puts earth.summary
   puts mercury.summary
 end
 
-main.summary
+main
+
+
+# # Wave 2
+# def main
+#   solar_system = SolarSystem.new('Sol')
+
+#   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+#   mercury = Planet.new('Mercury', 'red', 5.972e24, 1.496e8, 'It is the smallest planet?')
+
+#   solar_system.add_planet(earth)
+#   solar_system.add_planet(mercury)
+
+#   def summary
+#     return "For the #{@color} planet #{@name}, it has a mass of #{@mass_kg} kgs."
+#   end
+
+#   list = solar_system.list_planets
+#   puts list
+#   # => Planets orbiting Sol
+#   # => 1.  Earth
+# end
+
+# main
+
