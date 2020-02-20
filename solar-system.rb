ec2c123081f82f38465d483c095f4618bd71fc32
RebeccaRoach <rebeccaroach12@gmail.com>
2020-02-19 17:32:26 -0800

    Wave 3 requirements

--- solar-system.rb
+++ solar-system.rb
@@ -12,7 +12,7 @@ class SolarSystem
   end
 
   def list_planets
-    str_planets = "Planets orbiting #{@star_name}"
+    str_planets = "Planets orbiting #{@star_name}\n------------------"
     @planets.each_with_index do |planet, index|
       str_planets += "\n #{index+1}. #{planet.name}"
     end
@@ -26,7 +26,7 @@ class SolarSystem
         return planet
       end
     end
-    return "Sorry, that planet wasn't found."
+    return false
   end
 
   def distance_between(planet_1, planet_2)
@@ -34,6 +34,15 @@ class SolarSystem
     planet_2_distance = find_planet_by_name(planet_2).distance_from_sun_km 
     difference = planet_1_distance - planet_2_distance
     return "The distance between #{planet_1} and #{planet_2} is #{difference.abs} km."
+  end
+
+  def summarize_planet_details(named_planet)
+    found_planet = find_planet_by_name(named_planet)
+    if found_planet.class == Planet
+      return found_planet.summary
+    else
+      return "Sorry, no details available."
+    end
   end
 
 end
